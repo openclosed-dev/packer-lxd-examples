8f39e8b8b8f9a9cf601bea6f08d32b5900c453ae
leadpony <leadpony@openclosed.dev>
2025-05-25 18:15:56 +0900

    Process desktop files in locale-agnostic manner

--- ubuntu-gui-desktop-icon/scripts/user/add-desktop-icons.sh
+++ ubuntu-gui-desktop-icon/scripts/user/add-desktop-icons.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -eu
 
-mkdir -p $HOME/Desktop
+# Desktop directory for current user
+DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+
+mkdir -p $DESKTOP_DIR
 
 # Add .desktop file as an example.
-cat <<EOF > $HOME/Desktop/gedit.desktop
+cat <<EOF > $DESKTOP_DIR/gedit.desktop
 [Desktop Entry]
 Name=Text Editor
 Exec=/usr/bin/gedit %U
@@ -16,7 +19,7 @@ EOF
 
 # Make all desktop files executable and trusted.
 # Note that dbus-x11 package is required.
-for file in $HOME/Desktop/*.desktop; do
+for file in $DESKTOP_DIR/*.desktop; do
   chmod u+x $file
   dbus-launch gio set $file metadata::trusted true
 done
